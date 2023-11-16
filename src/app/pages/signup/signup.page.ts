8b9d4697c19438903c2f54b7f99e8e684b1977ae
RealJohnHarold <21-01210@g.batstate-u.edu.ph>
2023-11-16 20:36:11 +0800

    signup frontend

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, 
+  IonCardSubtitle, IonItem, IonInput, IonLabel, IonButton} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent, 
+    IonCardSubtitle, IonItem, IonInput, IonLabel, IonButton]
 })
 export class SignupPage implements OnInit {
 
