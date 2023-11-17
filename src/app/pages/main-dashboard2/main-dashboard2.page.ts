7ac5dd8c58f89fe2af8354118fae2a0fa3dc2ba7
RealJohnHarold <21-01210@g.batstate-u.edu.ph>
2023-11-17 23:20:02 +0800

    main dashboard 2

--- src/app/pages/main-dashboard2/main-dashboard2.page.ts
+++ src/app/pages/main-dashboard2/main-dashboard2.page.ts
@@ -1,21 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonicModule } from '@ionic/angular';
-
-import { IonToolbar, IonAccordion, IonItem, IonLabel, IonAccordionGroup,
-   IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle,
-  IonDatetime, IonFab, IonFabButton, IonIcon, IonButton} from '@ionic/angular/standalone';
-
+import {IonHeader, IonToolbar, IonTitle, IonButton, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonIcon, IonDatetime}from '@ionic/angular/standalone';
 @Component({
   selector: 'app-main-dashboard',
   templateUrl: './main-dashboard2.page.html',
   styleUrls: ['./main-dashboard2.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule,
-            IonToolbar, IonAccordion, IonItem, IonLabel, IonAccordionGroup,
-            IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle,
-            IonDatetime, IonFab, IonFabButton, IonIcon, IonButton]
+  imports: [IonHeader, IonToolbar, IonTitle, IonButton, 
+    IonContent, IonCard, IonCardContent, IonCardHeader, 
+    IonCardTitle, IonCardSubtitle, IonIcon, IonDatetime ]
+
 })
 export class MainDashboard2Page implements OnInit {
 
