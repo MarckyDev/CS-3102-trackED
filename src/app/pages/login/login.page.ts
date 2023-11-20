40ddda8cd7649785ced58aa78c4b5c63cc775012
GammyBear <gamzkee5@gmail.com>
2023-11-21 03:20:52 +0800

    BackEnd

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import {IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonInput, IonButton } from '@ionic/angular/standalone';
+import {IonContent,
+        IonCard,
+        IonCardHeader,
+        IonCardTitle,
+        IonCardContent,
+        IonItem,
+        IonInput,
+        IonButton
+       } from '@ionic/angular/standalone';
+
+
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-login',
@@ -22,11 +32,44 @@ import {IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonIte
     IonButton
   ]
 })
-export class LoginPage implements OnInit {
 
-  constructor() { }
+export class LoginPage{
+  key: any = "";
+  value: any = "";
 
-  ngOnInit() {
+  constructor(private databaseService: DatabaseService, private elementRef: ElementRef) { }
+
+  async setValue(){
+    this.getUsername();
+    this.getPassword();
+    await this.databaseService.set(this.key, this.value);
+    this.databaseService.viewStorage(); //in theory, it prints the storage 
+    
   }
+
+  async getKey(key_val:any){
+    await this.databaseService.get(key_val)
+  }
+
+
+  async getInputElement(class_name:any): Promise<HTMLInputElement>{
+    const IonInputElement = this.elementRef.nativeElement.querySelector(`.${class_name}`);
+    return await IonInputElement.getInputElement();
+  }
+
+  async getUsername(){
+    const value = this.getInputElement('email');
+    this.value = value;
+  }
+
+  async getPassword(){
+    const value = this.getInputElement('password');
+    this.key = value;
+  }
+
+  async clear(){
+    this.databaseService.clearStorage()
+  }
+
 
 }
---
+++ src/app/services/database.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  private _storage: Storage | null = null;
+
+  constructor(private storage: Storage) { }
+
+  async init(){
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  public async set(key: string, value: any){
+    let result = await this._storage?.set(key, value);
+    console.log(result);
+  }
+
+  public async get(key: string){
+    let value = await this._storage?.get(key);
+    console.log(value);
+    return value;
+  }
+
+  public async viewStorage(){
+    let value = await this._storage?.keys()
+    console.log(value);
+    return value;
+  }
+
+  public async clearStorage(){
+    await this._storage?.clear()
+  }
+
+
+
+}
