d1c01bd1c9b7c4fbb37889b7d54847a1df912254
jkgnc <21-02831@g.batstate-u.edu.ph>
2023-11-30 12:51:45 +0800

    notes updated

--- src/app/pages/notes/notes.page.ts
+++ src/app/pages/notes/notes.page.ts
@@ -1,20 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import {
+  IonToolbar,
+  IonAccordion,
+  IonItem,
+  IonLabel,
+  IonAccordionGroup,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonDatetime,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonButton,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.page.html',
   styleUrls: ['./notes.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    IonToolbar,
+    IonAccordion,
+    IonItem,
+    IonLabel,
+    IonAccordionGroup,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonDatetime,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonButton,
+  ],
 })
-export class NotesPage implements OnInit {
+export class NotesPage {
+  notes: any[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  addNote() {
+    // Implement the logic to add a new note
+    this.notes.push({});
+    // You might also perform additional logic or update other properties as needed.
   }
 
+  handleAddIconClick() {
+    // Implement the logic for the click event of the add icon
+    // You can add specific functionality here
+  }
 }
