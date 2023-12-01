8c7e95ca296e94b7e48b8c47660eff90dcf2260e
RealJohnHarold <21-01210@g.batstate-u.edu.ph>
2023-12-01 23:25:21 +0800

    to-do

--- src/app/pages/to-do/to-do.page.ts
+++ src/app/pages/to-do/to-do.page.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule} from '@ionic/angular';
-import { IonContent, IonLabel, IonCard, IonCardContent, IonTabBar, IonDatetime } from '@ionic/angular/standalone';
+import { IonContent, IonTabs, IonTabBar, IonTabButton, IonDatetime, IonCard, IonCardContent, IonLabel, IonSegment, IonSegmentButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.page.html',
   styleUrls: ['./to-do.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, IonContent, IonLabel, IonCard, IonCardContent, IonTabBar, IonDatetime]
+  imports: [IonicModule, CommonModule, FormsModule, IonContent, IonContent, IonTabs, IonTabBar, IonTabButton, IonDatetime, IonCard, IonCardContent, IonLabel, IonSegment, IonSegmentButton ]
 })
 export class ToDoPage implements OnInit {
 
