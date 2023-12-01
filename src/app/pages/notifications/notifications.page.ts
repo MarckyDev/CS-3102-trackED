3ca82f39c4f9999aabb5863bf88ca49053bc4244
RealJohnHarold <21-01210@g.batstate-u.edu.ph>
2023-12-01 14:11:14 +0800

    to-do

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { IonContent, IonSearchbar} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.page.html',
   styleUrls: ['./notifications.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, IonContent, IonSearchbar]
 })
 export class NotificationsPage implements OnInit {
 
