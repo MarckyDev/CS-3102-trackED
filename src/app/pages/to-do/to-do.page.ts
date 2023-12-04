e6f159215d2ff41e669c8055e0c37ff20658d3bd
RealJohnHarold <21-01210@g.batstate-u.edu.ph>
2023-12-05 00:22:03 +0800

    close-to-final

--- src/app/pages/to-do/to-do.page.ts
+++ src/app/pages/to-do/to-do.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule} from '@ionic/angular';
 import { IonContent, IonTabs, IonTabBar, IonTabButton, IonDatetime, IonCard, IonCardContent, IonLabel, IonSegment, IonSegmentButton } from '@ionic/angular/standalone';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-to-do',
@@ -13,7 +14,20 @@ import { IonContent, IonTabs, IonTabBar, IonTabButton, IonDatetime, IonCard, Ion
 })
 export class ToDoPage implements OnInit {
 
-  constructor() { }
+  first_name: any;
+  last_name: any;
+  constructor(private database: DatabaseService) {
+
+    this.parseData();
+   }
+
+   async parseData(){
+    let parseData = [];
+    let obj = JSON.parse(await this.database.get('Users'));
+    this.first_name = obj.firstName;
+    this.last_name = obj.lastName;
+
+   }
 
   ngOnInit() {
   }
